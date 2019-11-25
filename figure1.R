7bbcef1f3ee9de30ec7f71c55868cf8730a5964f
jujudu121212 <ferry@etud.insa-toulouse.fr>
2019-11-25 21:04:19 +0100

    Implements script for figure 1

--- figure1.R
+++ figure1.R
@@ -1,12 +1,19 @@
 library(ggplot2)
 library(plyr) # dplyr
+require(reshape2)
 # ou pour savoir lequel appeler:
 # dplyr::filter (par exemple)
 db = read.csv(file = "./data/subventions-accordees-et-refusees.csv", sep= ';')
 
+dbCountSummary = ddply(db, .(Année.budgétaire), summarize, nbAccept=sum(Montant.voté!="0"), nbRefuse=sum(Montant.voté=="0"))
+dbSummedTot = ddply(db, .(Année.budgétaire), summarize, sum_nb=sum(Montant.voté))
+dbCountMelt = melt(dbCountSummary, id.vars="Année.budgétaire")
 
-dbClean <- db[which(db$Montant.voté != "0"),]
-#dbSummed = ddply(dbClean, .(Année.budgétaire), summarize, sum_nb=sum(Nombre))
-
-ggplot(data = db, aes(x=factor(Année.budgétaire), y = sum_nb, colour = status) + geom_bar(stat = "identity", position="dodge");
-       
+ggplot(data = dbCountMelt, aes(x=factor(Année.budgétaire), y = value, fill = variable)) + 
+  geom_bar(stat = "identity", position="stack") + # ou dodge pour côte à côte 
+  scale_fill_manual(name="Nombre de demandes de subvention :", labels=c("Acceptées", "Refusées"), values=c("#74d712", "#f21e1e")) +
+  theme(legend.position = "bottom", legend.direction = "horizontal", 
+        legend.background = element_rect(fill="lightgray"), 
+        legend.key = element_rect(fill="black", color = NA), 
+        plot.title= element_text(hjust = 0.5), plot.subtitle = element_text(hjust = 0.5)) +
+  xlab("Année budgétaire") + ylab("Nombre de subventions") + labs(title = "Subventions à Paris")#, subtitle = "Evolution depuis 2013")
