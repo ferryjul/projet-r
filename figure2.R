f904f3f210a75de8db27e28ded55d5d28d2f2538
QuentinG66 <genoud@etud.insa-toulouse.fr>
2019-12-02 11:49:16 +0100

    maj figure2

--- figure2.R
+++ figure2.R
@@ -1,9 +1,15 @@
 library(ggplot2)
 library(plyr) # dplyr
-require(reshape2)
+
+library(tidyverse)
 # ou pour savoir lequel appeler:
 # dplyr::filter (par exemple)
 db = read.csv(file = "./data/subventions-accordees-et-refusees.csv", sep= ';')
+
+df=db
+
+
+df$Secteurs.d.activités.définies.par.l.association = strsplit((as.character(db$Secteurs.d.activités.définies.par.l.association)),";")
 
 dbCountSector = ddply(db, .(Secteurs.d.activités.définies.par.l.association), summarize, sum_nb=sum(Montant.voté))
 
@@ -13,5 +19,24 @@ strsplit("Aides aux associations;Architecture & urbanisme;Cu",";")
 
 act_sector = unique(unlist(strsplit((as.character(db$Secteurs.d.activités.définies.par.l.association)),";")))
 
-bSummedTot = ddply(db, .(act_sector), summarize, nbAccept=sum(Montant.voté!="0"))
+bSummedTot = ddply(db, .(act_sector), summarize, nbAccept=sum(Montant.voté))
+# 
+# newPrenomsDB = ddply(prenomsDB, .(Prénom, Année), function(x){
+#   if((x$Prénom[[1]] %in% dbFiveBest$Prénom)){
+#     if(x$Sexe[[1]] == dbFiveBest[which(dbFiveBest$Prénom == x$Prénom[[1]]),]$Sexe[[1]]){
+#       x$BestYear <- dbFiveBest[which(dbFiveBest$Prénom == x$Prénom[[1]]),]$Année[[1]]
+#       x
+#     }
+#   }
+# })
+
+#           
+which ((act_sector %in% df$Secteurs.d.activités.définies.par.l.association== TRUE))
+
+xaxa <- which((act_sector %in% c("Culture & Arts", "Education & formation", "Social")) == TRUE)
+
+
+ah = df %>%separate(Secteurs.d.activités.définies.par.l.association,c("s1","s2","s3"))
+
+
 
