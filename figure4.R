f4b47c060b6f6580e665b48a204c9e80167487e3
QuentinG66 <genoud@etud.insa-toulouse.fr>
2019-12-04 13:10:43 +0100

    clean code

--- figure4.R
+++ figure4.R
@@ -10,22 +10,24 @@ db = read.csv(file = "./data/subventions-accordees-et-refusees.csv", sep= ';')
 df <- db %>%
   transform(Secteurs.d.activités.définies.par.l.association = strsplit((as.character(Secteurs.d.activités.définies.par.l.association)),";")) %>% unnest(Secteurs.d.activités.définies.par.l.association)
 
-#réorganisation du dataset en fonction des secteurs + on calcule la montant moyen des subventions acceptées
+#réorganisation du dataset en fonction des secteurs 
 db_sector = ddply(df, .(Secteurs.d.activités.définies.par.l.association), summarize, Tot_Subv=sum(Montant.voté), proportion_sector = 1)
 
-
+#on détermine le montant total des subventions attribués à tous les secteurs et on l'ajoute au dataset
 total_subv = sum(db_sector$Tot_Subv)
 
 db_sector$proportion_sector = db_sector$Tot_Subv / total_subv
 
+#on enlève les secteurs à moins de 2,5%
 db_sector_filter <- db_sector %>% filter(proportion_sector >= 0.025)
 
 
-
+#on rajoute la section "Autres" qui correspond à l'ensemble secteurs filtrés
 db_sector_pie <- rbind(db_sector_filter,data.frame("Secteurs.d.activités.définies.par.l.association"= "Autres","Tot_Subv"=0,"proportion_sector"=1-sum(db_sector_filter$proportion_sector)))
 
 db_sector_pie$data_labeli <- scales::percent(db_sector_pie$proportion_sector)
   
+#on construit notre graphe
 ggplot(db_sector_pie)+
   geom_bar(aes(x="", y=proportion_sector, fill=Secteurs.d.activités.définies.par.l.association), width = 1, stat = "identity")+ coord_polar("y") +
   theme_void() + geom_text(aes(x= 1, y=proportion_sector, label = data_labeli), position=position_stack(vjust=0),size=4)
